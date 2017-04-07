d604beca78b8973b28604fec7ea33326f17514cb
Tadej Petek <petektadej98@gmail.com>
2017-04-07 12:29:28 +0200

    Backup Update

--- app/src/main/java/net/bounceme/memes/clicker/SplashScreen.java
+++ app/src/main/java/net/bounceme/memes/clicker/SplashScreen.java
@@ -1,6 +1,7 @@
 package net.bounceme.memes.clicker;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 
@@ -16,9 +17,22 @@ public class SplashScreen extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash);
+        Thread myThread = new Thread(){
+            @Override
+            public void run(){
+                try{
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }catch(InterruptedException e){
+                    e.printStackTrace();
 
+                }
+            }
+        };
+            myThread.start();
 
-        final ImageView iv = (ImageView) findViewById(R.id.imageView);
 
 
     }
